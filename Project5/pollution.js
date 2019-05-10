23b306f8eec1f6ccf42d1633e60df82afc3ba743
Jacquelyn Wu <nutella.monster233@gmail.com>
2019-05-10 13:05:58 -0400

    UPDATE

--- Project5/pollution.js
+++ Project5/pollution.js
@@ -17,9 +17,9 @@ var airquality //real-time airquality info
     function delhiAnimate(){
       var dot1 = document.querySelector('#delhiAPI')
       dot1.animate([
-        {transform: 'translateY('+airquality/20+'vw)'},
+        {transform: 'translateY('+airquality/10+'vw)'},
         {transform: 'translateY(0vw)'},
-        {transform: 'translateY('+airquality/20+'vw)'}
+        {transform: 'translateY('+airquality/10+'vw)'}
       ],{
         duration:6000,
         easing: 'ease-in-out',
